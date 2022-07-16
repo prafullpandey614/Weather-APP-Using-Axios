552fd209e7b6e6d918fdf2af7aa38131330d3933
Prafull <prafullpandey614@gmail.com>
2022-07-17 00:49:48 +0530

    All features added excpet Service Worker

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css'
 
 const App = ()=>{
     const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
     const search = async(e)=>{
         if(e.key === 'Enter'){
             const data = await fetchWeather(query)    
             console.log(data);
+            setWeather(data);
+            setQuery('');
         }
       }
    
@@ -23,6 +26,26 @@ const App = ()=>{
             }}
             onKeyPress ={search}
            />
+           {weather.main && (
+               <div className="city">
+                   <h2 className="city-name">
+                       <span>
+                       {weather.name}
+                       </span>
+                     
+                   <sup>{weather.sys.country}</sup>
+                   </h2>
+                   <div className="city-temp">
+                   {Math.round(weather.main.temp)}
+                   <sup>&deg;C</sup>
+               </div>
+               <div className="info">
+               <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+               <p>{weather.weather[0].description}</p>
+               </div>
+               </div>
+               
+           )}
        </div>
 
 
